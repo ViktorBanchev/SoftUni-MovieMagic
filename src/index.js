df2f8006903fff006301a9a2559807b6c1964826
Vikicha089 <vbanchevf@gmail.com>
2025-09-22 18:13:50 +0300

    Setup express-handlebars

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 import express from "express"
+import handlebars from "express-handlebars"
 
 const app = express();
 
+//setup handlebars
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs',
+}));
+
+app.set('view engine', 'hbs');
+
+app.set('views', 'src/views');
+
+//routes
+app.get("/", (req, res) => {
+    res.render('home', { layout: false })
+})
+
+
 app.listen(5000, () => {
-    console.log("Listening to https://localhost:5000")
-    
+    console.log("Listening to http://localhost:5000")
+
 })
 
 console.log("It works!");
