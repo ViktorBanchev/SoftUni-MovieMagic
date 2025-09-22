818d44ac1204957cef3c1bb5cfe2e823d1b0ed32
Vikicha089 <vbanchevf@gmail.com>
2025-09-22 23:30:05 +0300

    show static search page

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -22,5 +22,9 @@ movieController.get('/:movieId/details', (req, res) => {
     res.render("details", { movie })
 })
 
+movieController.get("/search", (req, res) => {
+    res.render("search");
+})
+
 
 export default movieController;
