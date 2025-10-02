8e8086fe0ade80f8d99057b69855de0714c5fe15
Vikicha089 <vbanchevf@gmail.com>
2025-10-02 21:06:32 +0300

    Sekect proper category on movie edit

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -74,14 +74,16 @@ movieController.get("/:movieId/delete", isAuth, async (req, res) => {
 
 });
 
-movieController.get("/:movieId/edit", async (req ,res) => {
+movieController.get("/:movieId/edit", isAuth, async (req ,res) => {
     const movieId = req.params.movieId;
     const movie = await movieService.getOne(movieId);
 
-    res.render("movies/edit", { movie })
+    const categoriesViewData = getMovieCategoryViewData(movie.category) 
+
+    res.render("movies/edit", { movie, categories: categoriesViewData })
 });
 
-movieController.post("/:movieId/edit", async (req ,res) => {
+movieController.post("/:movieId/edit", isAuth, async (req ,res) => {
     const movieId = req.params.movieId;
     const movieData = req.body;
 
@@ -89,6 +91,20 @@ movieController.post("/:movieId/edit", async (req ,res) => {
 
     res.redirect(`/movies/${movieId}/details`);
 })
+
+function getMovieCategoryViewData(selectedCategory) {
+    const categories = [
+        {value: '', label: 'Select Category'},
+        {value: 'tv-show', label: 'TV Show'},
+        {value: 'animation', label: 'Animation'},
+        {value: 'movie', label: 'Movie'},
+        {value: 'documentary', label: 'Documentary'},
+        {value: 'short-film', label: 'Short Film'},
+    ]
+
+    const viewData = categories.map(category => ({ ...category, selected: selectedCategory === category.value ? 'selected' : ''}));
+    return viewData;
+}
 
 
 export default movieController;
